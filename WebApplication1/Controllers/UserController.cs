c93d6868c13f021d5b58f2a55d076084eeabc5a3
Mohd Shakaib Ghazi <mohd.shakaibghazi@mphasis.com>
2023-09-14 11:57:38 +0530

    Second

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        static List<User> users = new List<User>
+            {
+            new User() { UserId=1,UserName="sam",UserPwd="sam@123",UserEmail="sam@yahoo.com"},
+            new User() { UserId=2,UserName="jam",UserPwd="jam@123",UserEmail="jam@yahoo.com"}
+            };
+        public IActionResult Index()
+        {
+
+            return View(users);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new User());
+        }
+        [HttpPost]
+        public IActionResult Create(User model)
+        { if(ModelState.IsValid)
+            {
+                users.Add(model);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
